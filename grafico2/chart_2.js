3f4b8123cc1721824484f44c19863a8dc9f9ab3d
paulitajordan <paulijordan02@gmail.com>
2023-04-12 10:10:42 -0300

    chart

---
+++ grafico2/chart_2.js
@@ -0,0 +1,60 @@
+const mapaFetch = d3.json('barrios-caba.geojson')
+const dataFetch = d3.dsv(';', '../data/147_vehiculos_mal_estacionados.csv', d3.autoType)
+
+Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
+  
+  /* Agrupamos reclamos x barrio */
+  const reclamosPorBarrio = d3.group(data, d => d.domicilio_barrio) // crea un Map
+  console.log('reclamosPorBarrio', reclamosPorBarrio)
+
+  
+  /* A cada feature del mapa le agregamos la prop DENUNCIAS */
+  barrios.features.forEach(d => {
+    let nombreBarrio = d.properties.BARRIO
+    let cantReclamos =  reclamosPorBarrio.get(nombreBarrio).length
+    d.properties.DENUNCIAS = cantReclamos
+
+    console.log(nombreBarrio + ': ' + cantReclamos)
+  })
+
+
+  /* Mapa Coroplético */
+  let chartMap = Plot.plot({
+    // https://github.com/observablehq/plot#projection-options
+    projection: {
+      type: 'mercator',
+      domain: barrios, // Objeto GeoJson a encuadrar
+    },
+    color: {
+      // Quantize continuo (cant. denuncias) -> discreto (cant. colores)
+      type: 'quantize', 
+      n: 7,
+      scheme: 'Blues',
+      label: 'Cantidad de denuncias',
+      legend: true,
+    },
+    marks: [
+      Plot.geo(barrios, {
+        fill: d => d.properties.DENUNCIAS,
+        stroke: 'gray',
+        title: d => `${d.properties.BARRIO}\n${d.properties.DENUNCIAS} denuncias`,
+      }),
+      Plot.text(
+        barrios.features,
+        Plot.centroid({
+          text: (d) => d.properties.BARRIO,
+          fill: "currentColor",
+          stroke: "white",
+          textAnchor: "center",
+          dx: 5,
+          filter: (d) => d.properties.DENUNCIAS > 400 //solo muestro el nombre de los barrios con denuncias mayores a 80
+        })
+      )
+    ],
+  })
+
+  /* Agregamos al DOM la visualización chartMap */
+  d3.select('#chart').append(() => chartMap)
+
+
+})
