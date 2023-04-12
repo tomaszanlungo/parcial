21aaf76a8ef0c96a8277776eb999798d39688e5e
paulitajordan <paulijordan02@gmail.com>
2023-04-12 10:31:55 -0300

    Update chart_3.js

---
+++ grafico3/chart_3.js
@@ -0,0 +1,10 @@
+const mapaFetch = d3.json('barrios-caba.geojson')
+const dataFetch = d3.dsv(';', '../data/147_vehiculos_mal_estacionados.csv', d3.autoType)
+
+Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
+  
+  /* Agrupamos reclamos x barrio */
+  const reclamosPorBarrio = d3.group(data, d => d.domicilio_barrio) // crea un Map
+  console.log('reclamosPorBarrio', reclamosPorBarrio)
+})
+
